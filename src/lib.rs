465793da336eebbfcc59438a24dcbc2bb298d04e
Artem Zhmaka SYS <artem.zhmaka@globallogic.com>
2020-11-05 18:30:50 +0200

    upgrade hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,61 @@ use std::borrow::Borrow;
 const INITIAL_NBUCKETS: usize = 1;
 
 
+pub struct OccupiedEntry<'a, K: 'a, V: 'a> {
+    entry: &'a mut (K, V),
+}
+
+pub struct VacantEntry<'a, K: 'a, V: 'a> {
+    key: K,
+    map: &'a mut HashMap<K, V>,
+    bucket: usize,
+}
+
+impl<'a, K: 'a, V: 'a> VacantEntry<'a, K, V> {
+    pub fn insert(self, value: V) -> &'a mut V
+        where
+            K: Hash + Eq
+    {
+
+        self.map.buckets[self.bucket].push((self.key, value));
+        self.map.items += 1;
+        &mut self.map.buckets[self.bucket].last_mut().unwrap().1
+    }
+}
+
+pub enum Entry<'a, K, V> {
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K, V> Entry<'a, K, V>
+    where
+        K: Hash + Eq
+{
+    pub fn or_insert(self, value: V) -> &'a mut V {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(value)
+        }
+    }
+
+    pub fn or_insert_with<F>(self, maker: F) -> &'a mut V
+        where F: FnOnce() -> V
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(maker())
+        }
+    }
+
+    pub fn or_default(self) -> &'a mut V
+        where
+            V: Default,
+    {
+        self.or_insert_with(Default::default)
+    }
+}
+
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
     items: usize,
@@ -43,6 +98,28 @@ impl<K, V> HashMap<K, V>
         bucket.push((key, value));
         None
     }
+
+    pub fn entry(&mut self, key: K) -> Entry<K, V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
+        }
+
+
+        let bucket_num = self.bucket(&key);
+
+        for entry in &mut self.buckets[bucket_num] {
+            if entry.0 == key {
+                return Entry::Occupied(OccupiedEntry { entry: unsafe { &mut *(entry as *mut _) } });
+            }
+        }
+
+        Entry::Vacant(VacantEntry {
+            key,
+            map: self,
+            bucket: bucket_num,
+        })
+    }
+
 
     pub fn get<Q>(&self, key: &Q) -> Option<&V>
         where
